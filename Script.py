5cc7dbb83508147092113481c68e8e02d6c3d6fe
HDMoviesEarth <diesengaming517@gmail.com>
2025-05-31 21:01:55 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -3,9 +3,9 @@ class text(object):
 
 ɪ ᴄᴀɴ ɢᴇɴᴇʀᴀᴛᴇ ᴘʏʀᴏɢʀᴀᴍ ᴀɴᴅ ᴛᴇʟᴇᴛʜᴏɴ ꜱᴛʀɪɴɢ ꜱᴇꜱꜱɪᴏɴ
 
-nᴜꜱᴇ ʙᴇʟᴏᴡ ʙᴜᴛᴛᴏɴ ᴛᴏ sᴛᴀʀᴛ ɢᴇɴᴇʀᴀᴛɪɴɢ sᴛʀɪɴɢ sᴇssɪᴏɴ
+ᴘʀᴇss ʙᴇʟᴏᴡ ʙᴜᴛᴛᴏɴ ᴛᴏ sᴛᴀʀᴛ ɢᴇɴᴇʀᴀᴛɪɴɢ sᴛʀɪɴɢ sᴇssɪᴏɴ
 
-<blockquote>ᴍᴀɪɴᴛᴀɪɴᴇᴅ ʙʏ : <a href='https://telegram.me/TechifyRahul'>ʀᴀʜᴜʟ</a></blockquote></b>"""
+<blockquote>ᴍᴀɪɴᴛᴀɪɴᴇᴅ ʙʏ : <a href='https://t.me/ANKAN_Contact_BOT'>ᴀɴᴋᴀɴ</a></blockquote></b>"""
 
   LOG = """#New_User
 
@@ -15,17 +15,17 @@ ID: {}"""
   ABOUT = """<b>📜 Cʜᴇᴄᴋ Aʙᴏᴜᴛ:
   
 Lɪʙʀᴀʀʏ: Pʏʀᴏɢʀᴀᴍ 📚
-Lᴀɴɢᴜᴀɢᴇ: Pʏᴛʜᴏɴ 🐍
-Sᴇʀᴠᴇʀ: Rᴇɴᴅᴇʀ 🌐
-Bᴜɪʟᴅ Sᴛᴀᴛᴜs: V1.3 🚀
-Sᴏᴜʀᴄᴇ Cᴏᴅᴇ: Aᴠᴀɪʟᴀʙʟᴇ (Fʀᴇᴇ) 💻 
+Lᴀɴɢᴜᴀɢᴇ: ᴊᴀᴠᴀ 🧑‍💻
+Sᴇʀᴠᴇʀ: ʜᴇʀᴜᴋᴏ 🌐
+Bᴜɪʟᴅ Sᴛᴀᴛᴜs: V4.7 🚀
+Sᴏᴜʀᴄᴇ Cᴏᴅᴇ: (ᴘᴀɪᴅ) 💸
 
-<blockquote>ᴍᴀɪɴᴛᴀɪɴᴇᴅ ʙʏ : <a href='https://telegram.me/TechifyRahul'>ʀᴀʜᴜʟ</a></blockquote></b>"""
+<blockquote>ᴍᴀɪɴᴛᴀɪɴᴇᴅ ʙʏ : <a href='https://telegram.me/ANKAN_Contact_BOT'>ᴀɴᴋᴀɴ</a></blockquote></b>"""
   
   HELP = """<b>{},
 
-ᴛʜɪꜱ ɪꜱ ʀᴇᴀʟʟʏ sɪᴍᴘʟᴇ 😂
+ᴛʜɪꜱ ɪꜱ ʀᴇᴀʟʟʏ sɪᴍᴘʟᴇ 🤣
 
 ᴊᴜsᴛ ɢᴏ ʙᴀᴄᴋ ᴀɴᴅ ᴄʟɪᴄᴋ ᴏɴ ɢᴇɴᴇʀᴀᴛᴇ sᴛʀɪɴɢ sᴇssɪᴏɴ ʙᴜᴛᴛᴏɴ ᴀɴᴅ ᴛʜᴇɴ ꜰᴏʟʟᴏᴡ ᴛʜᴇ sᴛᴇᴘs
 
-<blockquote>ᴍᴀɪɴᴛᴀɪɴᴇᴅ ʙʏ : <a href='https://telegram.me/TechifyRahul'>ʀᴀʜᴜʟ</a></blockquote></b>"""
+<blockquote>ᴍᴀɪɴᴛᴀɪɴᴇᴅ ʙʏ : <a href='https://telegram.me/ANKAN_Contact_BOT'>ᴀɴᴋᴀɴ</a></blockquote></b>"""
