5779016c14860690b7a248784ab947dbfab4af60
HDMoviesEarth <diesengaming517@gmail.com>
2025-07-05 11:54:27 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -2,7 +2,7 @@ class text(object):
   START = """<b>
 ɪ ᴀᴍ ᴜʜᴅ <a href='https://t.me/UHD_StringSessionBot'>sᴛʀɪɴɢ sᴇssɪᴏɴ ʙᴏᴛ</a>, ɪ ᴄᴀɴ ɢᴇɴᴇʀᴀᴛᴇ ᴘʏʀᴏɢʀᴀᴍ ᴀɴᴅ ᴛᴇʟᴇᴛʜᴏɴ ꜱᴛʀɪɴɢ ꜱᴇꜱꜱɪᴏɴ,\n\nᴄʟɪᴄᴋ ʙᴇʟᴏᴡ ʙᴜᴛᴛᴏɴ ᴛᴏ sᴛᴀʀᴛ ɢᴇɴᴇʀᴀᴛɪɴɢ sᴛʀɪɴɢ sᴇssɪᴏɴ..!
 
-<blockquote>ᴍᴀɪɴᴛᴀɪɴᴇᴅ ʙʏ : <a href='https://t.me/ANKAN_Contact_BOT'>ᴀɴᴋᴀɴ</a></blockquote></b>"""
+<blockquote>ᴍᴀᴅᴇ ʙʏ : <a href='https://t.me/UHD_Bots'>ᴜʜᴅ ʙᴏᴛs™</a></blockquote></b>"""
 
   LOG = """#New_User
 
